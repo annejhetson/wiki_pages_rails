60c7420d1b15ef08180c40448f4ba1e380713a0f
Liz Tom & Anne Jhetson <student@epicodus.com>
2014-03-31 09:02:22 -0700

    start new rails project

---
+++ wikipages/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wikipages::Application.config.secret_key_base = 'd3210235160c70c5288d32a4293d90d111fea462e161289acbb77ef248133bfb2c9d8bf440d801bebb39e1d4e9ec20d3c20e92fb5496e8b09bd504e9c2c70699'
